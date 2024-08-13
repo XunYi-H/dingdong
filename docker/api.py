fc99228589dcce0cdf64f4e024bc19887f711984
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-08-13 17:55:15 +0800

    1

--- docker/api.py
+++ docker/api.py
@@ -88,7 +88,7 @@ async def index():
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
-
+    return response
     """
     response = requests.get('https://888.88:9/vilate')
 
